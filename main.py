f8ca9ac4c7fd2008ffd3907e9daaf74aa502e63b
Paolo Poemape <paolopoemape@gmail.com>
2021-09-14 16:33:07 -0600

    finished

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ governor: one who governs \
 translator: one who translates\
 Hiking can be arduous.\
 Writing is not for sissies.\
-Loitering is not permitted.'
+Loitering is not permitted.\
+legatee: the person who is to receive a legacy payee: the person who has the right to be paid \
+employee: one who is employed evacuee: one who is evacuated parolee: one who is paroled agreement, from “to agree” \
+acceptance, from “to accept” \
+conference, from “to confer” \
+failure, from “to fail” '
+
 
 #ing
 ing = re.findall(r"\w+ing\b", text_analyzed, flags=re.I)
@@ -27,3 +33,18 @@ print(ion)
 #or
 find_or = re.findall(r"\w+or\b", text_analyzed, flags=re.I)
 print(find_or)
+
+#ee
+ee = re.findall(r"\w+ee\b", text_analyzed, flags=re.I)
+print(ee)
+
+#ment
+ment = re.findall(r"\w+ment\b", text_analyzed, flags=re.I)
+print(ment)
+
+#ence or ance
+
+ence_or_ance = re.findall(r"\w+b?nce\b", text_analyzed, flags=re.I)
+print(ence_or_ance)
+
+
